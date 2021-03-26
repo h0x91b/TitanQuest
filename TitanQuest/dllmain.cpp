e1278de4fdd4273a60fabeda6bf2e41a07c6ea09
h0x91B <h0x91b@gmail.com>
2021-03-26 21:28:45 +0300

    INIT

---
+++ TitanQuest/dllmain.cpp
@@ -0,0 +1,330 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <detours.h>
+
+#include <Windows.h>
+#include <d3d9.h>
+
+#include "imgui.h"
+#include "imgui_impl_dx9.h"
+#include "imgui_impl_win32.h"
+
+#include "Character.h"
+#include "Engine.dll.h"
+#include "dx.h"
+
+static VOID(WINAPI* TrueSleep)(DWORD dwMilliseconds) = Sleep;
+void* ByPtr(DWORD base, DWORD offset, ...);
+HMODULE engineDll = 0;
+
+typedef HRESULT(__stdcall* EndScene) (IDirect3DDevice9*);
+EndScene realEndScene = nullptr;
+
+typedef HRESULT(__stdcall* Present) (IDirect3DDevice9*, const RECT* pSourceRect, const RECT* pDestRect, HWND hDestWindowOverride, const RGNDATA* pDirtyRegion);
+Present realPresent = nullptr;
+
+// virtual HRESULT __stdcall IDirect3DDevice9::Reset(D3DPRESENT_PARAMETERS * pPresentationParameters)
+typedef HRESULT(__stdcall* Reset) (IDirect3DDevice9*, D3DPRESENT_PARAMETERS* pPresentationParameters);
+Reset realReset = nullptr;
+
+typedef float (__fastcall GetCurrentMana)(Character* _this, DWORD edx);
+GetCurrentMana* realGetCurrentMana = nullptr;
+
+typedef float (__fastcall CharacterAddMoney)(Character* _this, DWORD edx, uint money);
+CharacterAddMoney* realCharacterAddMoney = nullptr;
+
+typedef uint (__fastcall GetItemCost)(void* _this, DWORD edx, bool p1);
+GetItemCost* realGetItemCost = nullptr;
+
+Engine* pEngine = nullptr;
+static LPDIRECT3D9              g_pD3D = NULL;
+static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
+
+DWORD* pVTable;
+HWND hwnd;
+
+bool show_demo_window = true;
+WNDPROC originalWndProc = nullptr;
+// Forward declare message handler from imgui_impl_win32.cpp
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+static LRESULT __stdcall _wndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (ImGui_ImplWin32_WndProcHandler(window, msg, wParam, lParam))
+        return true;
+    return CallWindowProc(originalWndProc, window, msg, wParam, lParam);
+}
+
+VOID WINAPI TimedSleep(DWORD dwMilliseconds)
+{
+    WCHAR buf[256];
+    wsprintf(buf, L"sleep(%d)\r\n", dwMilliseconds);
+    OutputDebugString(buf);
+    TrueSleep(dwMilliseconds);
+}
+
+bool godMode = 0;
+int frame = 0;
+
+HRESULT __stdcall _Present(IDirect3DDevice9* d, const RECT* pSourceRect, const RECT* pDestRect, HWND hDestWindowOverride, const RGNDATA* pDirtyRegion) {
+    //OutputDebugString(L"_Present\r\n");
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Start the Dear ImGui frame
+    ImGui_ImplDX9_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    //if (show_demo_window) {
+    //    ImGui::ShowDemoWindow(&show_demo_window);
+    //}
+
+    if (frame++ % 10 == 0) {
+        BYTE* pGodMode = (BYTE*)ByPtr((DWORD)engineDll, 0x00365DF0, 0x218, 0x74, 0xc25, -1);
+        if (pGodMode) {
+            *pGodMode = godMode;
+        }
+    }
+
+    {
+        ImGui::Begin("The tool", 0, ImGuiWindowFlags_MenuBar);
+
+        ImGui::Checkbox("God mode", &godMode);
+        //if (ImGui::BeginMenuBar())
+        //{
+        //    if (ImGui::BeginMenu("File"))
+        //    {
+        //        if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+        //        if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+        //        if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
+        //        ImGui::EndMenu();
+        //    }
+        //    ImGui::EndMenuBar();
+        //}
+
+        //// Edit a color (stored as ~4 floats)
+        //ImGui::ColorEdit4("Color", my_color);
+
+        //// Plot some values
+        //const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
+        //ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
+
+        //// Display contents in a scrolling region
+        //ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
+        //ImGui::BeginChild("Scrolling");
+        //for (int n = 0; n < 50; n++)
+        //    ImGui::Text("%04d: Some text", n);
+        //ImGui::EndChild();
+        ImGui::End();
+    }
+
+    // Rendering
+    ImGui::EndFrame();
+
+    g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
+    g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+    g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+    D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 255.0f), (int)(clear_color.y * clear_color.w * 255.0f), (int)(clear_color.z * clear_color.w * 255.0f), (int)(clear_color.w * 255.0f));
+    // g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+    if (g_pd3dDevice->BeginScene() >= 0)
+    {
+        ImGui::Render();
+        ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+        g_pd3dDevice->EndScene();
+    }
+    return realPresent(d, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion);
+}
+
+HRESULT __stdcall _Reset(IDirect3DDevice9* d, D3DPRESENT_PARAMETERS* pPresentationParameters) {
+    OutputDebugString(L"_Reset\r\n");
+
+    ImGui_ImplDX9_InvalidateDeviceObjects();
+    auto result = realReset(d, pPresentationParameters);
+    ImGui_ImplDX9_CreateDeviceObjects();
+    
+    return result;
+}
+
+HRESULT __stdcall _EndScene(IDirect3DDevice9* d) {
+    //OutputDebugString(L"_EndScene\r\n");
+    return realEndScene(d);
+}
+
+float __fastcall _GetCurrentMana(Character* _this, DWORD edx) {
+    // OutputDebugString(L"_GetCurrentMana\r\n");
+    // _this->health = 150.0f;
+    return realGetCurrentMana(_this, edx) - 50;
+}
+
+float __fastcall _CharacterAddMoney(Character* _this, DWORD edx, uint money) {
+    OutputDebugString(L"_CharacterAddMoney\r\n");
+    return realCharacterAddMoney(_this, edx, money * 15);
+}
+
+uint __fastcall _GetItemCost(void* _this, DWORD edx, bool p1) {
+    OutputDebugString(L"_GetItemCost\r\n");
+
+    return 50000;
+    //return realCharacterAddMoney(_this, edx, money * 15);
+}
+
+void* ByPtr(DWORD base, DWORD offset, ...)
+{
+    DWORD a = (base + offset);
+    DWORD tmp;
+    va_list vl;
+    va_start(vl, offset);
+    while (true)
+    {
+        tmp = va_arg(vl, DWORD);
+        if (tmp == -1) break;
+        
+        if (IsBadWritePtr((DWORD*)a, 4)) {
+            a = 0;
+            break;
+        }
+        
+        a = *(DWORD*)a;
+        if (a == 0) {
+            break;
+        }
+        a += tmp;
+    }
+    va_end(vl);
+    return (void*)a;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  dwReason,
+                       LPVOID lpReserved
+                     )
+{
+    if (DetourIsHelperProcess()) {
+        return TRUE;
+    }
+
+    if (dwReason == DLL_PROCESS_ATTACH) {
+//        MessageBox(0, L"Injected DLL", L"Inject", 0);
+        DetourRestoreAfterWith();
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+
+        HMODULE gameDll = GetModuleHandleA("game.dll");
+        engineDll = GetModuleHandleA("Engine.dll");
+        realGetCurrentMana = (GetCurrentMana*)GetProcAddress(gameDll, MAKEINTRESOURCEA(8427));
+        realCharacterAddMoney = (CharacterAddMoney*)GetProcAddress(gameDll, MAKEINTRESOURCEA(5345));
+        realGetItemCost = (GetItemCost*)GetProcAddress(gameDll, MAKEINTRESOURCEA(9000));
+        pEngine = (Engine*)GetProcAddress(gameDll, MAKEINTRESOURCEA(5377));
+
+        // "Engine.dll"+00365DF0 - pEngine
+
+        // "Engine.dll"+00366740
+
+        //void *ptr = (void*)(*(DWORD*)((DWORD)engineDll + 0x00366740));
+
+        // way #1
+        // dx_s1* dx = (dx_s1*) *(DWORD*) ((DWORD)engineDll + 0x00366740);
+        
+        // way #2
+
+        dx_s1* dx = *(dx_s1**) ((DWORD)engineDll + 0x00366740);
+        dx_s2* dx2 = *(dx_s2**) ((DWORD)engineDll + 0x00365E48);
+
+
+        //DWORD a = ((DWORD)engineDll + 0x00365DF0);
+        //a = *(DWORD*)a;
+        //a += 0x218;
+        //a = *(DWORD*)a;
+        //a += 0x74;
+        //a = *(DWORD*)a;
+        //a += 0xc25;
+
+        //BYTE* target = (BYTE*)a;
+
+        //BYTE* target2 = (BYTE*)ByPtr((DWORD)engineDll, 0x00365DF0, 0x218, 0x74, 0xc25, -1);
+
+        //a += 0x218;
+    //a = *(DWORD*)a;
+        
+//        b = (*(DWORD*)a + 0x218)
+        
+        // *(BYTE*)(*(DWORD*)(*(DWORD*) ((DWORD)engineDll + 0x00365DF0) + 0x218) + 0x74) + 0x25c)
+        //"Engine.dll" + 00365DF0
+        //218
+        //74
+        //    25c <<<<< byte
+//
+////        dx_s1* dx = (dx_s1*) *(DWORD*)(engineDll + 0x00366740);
+        g_pD3D = (LPDIRECT3D9)dx->g_pD3D;
+        g_pd3dDevice = (LPDIRECT3DDEVICE9)dx2->g_pd3dDevice;
+
+        //auto hwnd = CreateWindow(L"STATIC", L"Dummy window", 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        hwnd = FindWindow(NULL, L"Titan Quest Anniversary Edition");
+
+        //originalWndProc = WNDPROC(SetWindowLongPtrW(hwnd, GWLP_WNDPROC, LONG_PTR(_wndProc)));
+        originalWndProc = reinterpret_cast<WNDPROC>(SetWindowLongA(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(_wndProc)));
+
+        pVTable = *reinterpret_cast<DWORD**>(g_pd3dDevice);
+
+//        realEndScene = (EndScene)pVTable[42];
+        realReset = (Reset)pVTable[16];
+        realPresent = (Present)pVTable[17];
+  //      pVTable[42] = (DWORD)_EndScene;
+        pVTable[17] = (DWORD)_Present;
+        pVTable[16] = (DWORD)_Reset;
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+
+        ImGui_ImplWin32_Init(hwnd);
+        ImGui_ImplDX9_Init(g_pd3dDevice);
+
+        io.Fonts->AddFontDefault();
+
+        //io.Fonts->Build();
+        
+        DetourAttach(&(PVOID&)realGetCurrentMana, _GetCurrentMana);
+        DetourAttach(&(PVOID&)realCharacterAddMoney, _CharacterAddMoney);
+        DetourAttach(&(PVOID&)realGetItemCost, _GetItemCost);
+        //DetourAttach((PVOID*)pVTable[17], _Present);
+        //DetourAttach((PVOID*)pVTable[16], _Reset);
+        //DetourAttach(&(LPVOID&)pVTable[42], _EndScene);
+        DetourTransactionCommit();
+    }
+    else if (dwReason == DLL_PROCESS_DETACH) {
+        ImGui_ImplDX9_Shutdown();
+        ImGui_ImplWin32_Shutdown();
+        ImGui::DestroyContext();
+
+        SetWindowLongA(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(originalWndProc));
+
+        // SetWindowLongPtrW(hwnd, GWLP_WNDPROC, LONG_PTR(originalWndProc));
+        // WNDPROC(SetWindowLongPtrW(hwnd, GWLP_WNDPROC, LONG_PTR(originalWndProc)));
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        // DetourDetach(&(PVOID&)TrueSleep, TimedSleep);
+        DetourDetach(&(PVOID&)realGetCurrentMana, _GetCurrentMana);
+        DetourDetach(&(PVOID&)realCharacterAddMoney, _CharacterAddMoney);
+        DetourDetach(&(PVOID&)realGetItemCost, _GetItemCost);
+//        DetourDetach((PVOID*)pVTable[17], _Present);
+//        DetourDetach((PVOID*)pVTable[16], _Reset);
+        // DetourDetach(&(PVOID&)TrueSleep, TimedSleep);
+        DetourTransactionCommit();
+
+    //    pVTable[42] = (DWORD)realEndScene;
+        pVTable[17] = (DWORD)realPresent;
+        pVTable[16] = (DWORD)realReset;
+    }
+
+    return TRUE;
+}
+
---
+++ TitanQuest/dx.h
@@ -0,0 +1,76 @@
+typedef unsigned char   undefined;
+
+typedef unsigned int    ImageBaseOffset32;
+typedef unsigned char    byte;
+typedef unsigned int    dword;
+typedef long long    longlong;
+typedef unsigned char    uchar;
+typedef unsigned int    uint;
+typedef unsigned long    ulong;
+typedef unsigned long long    ulonglong;
+typedef unsigned char    undefined1;
+typedef unsigned short    undefined2;
+typedef unsigned int    undefined4;
+typedef unsigned long long    undefined8;
+typedef unsigned short    ushort;
+typedef unsigned short    word;
+typedef struct dx_s1 dx_s1, *Pdx_s1;
+
+struct dx_s1 {
+    undefined field_0x0;
+    undefined field_0x1;
+    undefined field_0x2;
+    undefined field_0x3;
+    undefined field_0x4;
+    undefined field_0x5;
+    undefined field_0x6;
+    undefined field_0x7;
+    undefined field_0x8;
+    undefined field_0x9;
+    undefined field_0xa;
+    undefined field_0xb;
+    undefined field_0xc;
+    undefined field_0xd;
+    undefined field_0xe;
+    undefined field_0xf;
+    undefined field_0x10;
+    undefined field_0x11;
+    undefined field_0x12;
+    undefined field_0x13;
+    undefined field_0x14;
+    undefined field_0x15;
+    undefined field_0x16;
+    undefined field_0x17;
+    undefined field_0x18;
+    undefined field_0x19;
+    undefined field_0x1a;
+    undefined field_0x1b;
+    undefined field_0x1c;
+    undefined field_0x1d;
+    undefined field_0x1e;
+    undefined field_0x1f;
+    undefined field_0x20;
+    undefined field_0x21;
+    undefined field_0x22;
+    undefined field_0x23;
+    undefined field_0x24;
+    undefined field_0x25;
+    undefined field_0x26;
+    undefined field_0x27;
+    void * g_pD3D;
+};
+
+typedef struct dx_s2 dx_s2, *Pdx_s2;
+
+struct dx_s2 {
+    undefined field_0x0;
+    undefined field_0x1;
+    undefined field_0x2;
+    undefined field_0x3;
+    undefined field_0x4;
+    undefined field_0x5;
+    undefined field_0x6;
+    undefined field_0x7;
+    void * g_pd3dDevice;
+};
+
